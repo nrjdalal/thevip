38b71587520bd40d362f10f01f30f7ff05f6719d
Neeraj Dalal <admin@nrjdalal.com>
2021-08-26 17:41:58 +0530

    temp push

--- pages/index.jsx
+++ pages/index.jsx
@@ -48,7 +48,7 @@ const index = () => {
       </div>
 
       {/* notice */}
-      <div className="text-sky-300 max-w-screen-md mx-auto mt-10 md:px-4">
+      <div className="text-sky-300 max-w-screen-md mx-auto mt-10 px-4">
         Sir furthur work requires constant interaction bw us. Please be
         available on Fiverr or contact us via information given below.
         <br />
