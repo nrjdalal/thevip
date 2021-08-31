872a892b473ce71df69443055f84635e150388c5
Neeraj Dalal <admin@nrjdalal.com>
2021-08-31 21:08:35 +0530

    artist render

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,8 @@
-import Countdown from 'react-countdown'
-
 import { PreRelease } from 'components/Artist'
 
 const index = () => {
   return (
     <>
-      <Countdown date={'2021-09-11T10:00:00.000Z'} />
       <PreRelease />
     </>
   )
