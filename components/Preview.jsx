5907fe049a52903024349e37d851821b6d71b5d5
Neeraj Dalal <admin@nrjdalal.com>
2021-09-16 19:55:56 +0530

    button added

--- components/Preview.jsx
+++ components/Preview.jsx
@@ -91,6 +91,21 @@ export const Preview = (props) => {
             style={isStart ? { display: 'none' } : { display: 'flex' }}
             className="absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex-col items-center text-center w-full p-6 h-full justify-center"
           >
+            <div className="mb-6 bg-white h-6 w-6 flex items-center justify-center rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-[#18a0aa]"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+
             <div className="font-montserrat font-semibold mb-6 text-sm">
               Please, read before pressing play
             </div>
