6af4a2a02d5317d7ac55c27a62b631b0d72cab27
Neeraj Dalal <admin@nrjdalal.com>
2021-09-16 19:46:12 +0530

    date management like pro

--- components/Preview.jsx
+++ components/Preview.jsx
@@ -64,6 +64,83 @@ export const Preview = (props) => {
     }
   }
 
+  const PreButton = () => {
+    if (currentDate < preDate) {
+      return (
+        <>
+          <div className="aspect-w-1 aspect-h-1">
+            <img
+              className="object-center object-cover"
+              src={props.cover}
+              alt=""
+            />
+          </div>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <div className="aspect-w-1 aspect-h-1 filter brightness-50 contrast-50">
+            <img
+              className="object-center object-cover"
+              src={props.cover}
+              alt=""
+            />
+          </div>
+          <div
+            style={isStart ? { display: 'none' } : { display: 'flex' }}
+            className="absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex-col items-center text-center w-full p-6 h-full justify-center"
+          >
+            <div className="font-montserrat font-semibold mb-6 text-sm">
+              Please, read before pressing play
+            </div>
+
+            <div className="mb-6 text-sm">
+              After pressing play, you will be able to listen to the music only
+              once.
+            </div>
+
+            <div
+              onClick={toggleStart}
+              className="bg-white text-black rounded-md p-1.5 px-3 text-xs font-nunitoSans font-semibold cursor-pointer"
+            >
+              Take me to the pre-release
+            </div>
+          </div>
+          {/* streamer */}
+          <div
+            style={isStart ? { display: 'flex' } : { display: 'none' }}
+            className="absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex-col items-center text-center w-full h-full justify-center"
+          >
+            {isStart ? (
+              <Stream
+                src={videoIdOrSignedUrl}
+                className="w-full h-full"
+                autoplay={true}
+                preload="metadata"
+                onEnded={toggleEnded}
+              />
+            ) : (
+              'Video ready to be played.'
+            )}
+          </div>
+
+          {/* after playback */}
+          <div
+            style={isEnded ? { display: 'flex' } : { display: 'none' }}
+            className="absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex-col items-center text-center w-full h-full justify-center aspect-w-1 aspect-h-1"
+          >
+            <img
+              className="object-cover object-center"
+              src="https://optinmonster.com/wp-content/uploads/2016/10/Anatomy-of-the-Perfect-Thank-You-Page.png"
+              alt=""
+            />
+          </div>
+        </>
+      )
+    }
+  }
+
   return (
     <>
       <div className="bg-raisenBlack py-10 md:py-40">
@@ -71,65 +148,7 @@ export const Preview = (props) => {
         <div className="max-w-screen-md mx-auto flex flex-col md:flex-row md:px-4">
           {/* cover */}
           <div className="relative md:w-1/2 md:order-2 md:ml-2">
-            <div className="aspect-w-1 aspect-h-1 filter brightness-50 contrast-50">
-              <img
-                className="object-center object-cover"
-                src={
-                  props.cover ||
-                  'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Solid_white.svg/2048px-Solid_white.svg.png'
-                }
-                alt=""
-              />
-            </div>
-            <div
-              style={isStart ? { display: 'none' } : { display: 'flex' }}
-              className="absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex-col items-center text-center w-full p-6 h-full justify-center"
-            >
-              <div className="font-montserrat font-semibold mb-6 text-sm">
-                Please, read before pressing play
-              </div>
-
-              <div className="mb-6 text-sm">
-                After pressing play, you will be able to listen to the music
-                only once.
-              </div>
-
-              <div
-                onClick={toggleStart}
-                className="bg-white text-black rounded-md p-1.5 px-3 text-xs font-nunitoSans font-semibold cursor-pointer"
-              >
-                Take me to the pre-release
-              </div>
-            </div>
-            {/* streamer */}
-            <div
-              style={isStart ? { display: 'flex' } : { display: 'none' }}
-              className="absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex-col items-center text-center w-full h-full justify-center"
-            >
-              {isStart ? (
-                <Stream
-                  src={videoIdOrSignedUrl}
-                  className="w-full h-full"
-                  autoplay={true}
-                  preload="metadata"
-                  onEnded={toggleEnded}
-                />
-              ) : (
-                'Video ready to be played.'
-              )}
-            </div>
-
-            {/* after playback */}
-            <div
-              style={isEnded ? { display: 'flex' } : { display: 'none' }}
-              className="absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex-col items-center text-center w-full h-full justify-center aspect-w-1 aspect-h-1"
-            >
-              <img
-                className="object-cover object-center"
-                src="https://optinmonster.com/wp-content/uploads/2016/10/Anatomy-of-the-Perfect-Thank-You-Page.png"
-                alt=""
-              />
-            </div>
+            <PreButton />
           </div>
           {/* song info */}
           <div className="ml-4 md:ml-0 mt-6 md:mt-0 md:w-1/2 md:order-1 md:mr-2 text-white font-montserrat text-xl">
