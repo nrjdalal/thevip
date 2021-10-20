b941dd456d59347e8fea19bab755705ea06053b2
Neeraj Dalal <admin@nrjdalal.com>
2021-10-20 23:25:06 +0530

    linked to home

--- components/Text.jsx
+++ components/Text.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
+import Link from 'next/link'
 
 const Text = (props) => {
   return (
     <>
       <div className="py-20 bg-white">
         <div className="flex justify-center pb-6">
-          <img className="w-32" src="/kloov_logo_black.svg" alt="" />
+          <Link href="/">
+            <a>
+              <img className="w-32" src="/kloov_logo_black.svg" alt="" />
+            </a>
+          </Link>
         </div>
 
         <div className="container px-4 mx-auto prose max-w-none md:w-2/3 font-montserrat">
